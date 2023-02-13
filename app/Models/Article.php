cbf0abe72449eb3168f24bb9fc16cd90082f7a07
semenov.d <semenov.d@introvert.bz>
2023-02-13 22:45:52 +0300

    added seeds for articles

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Article extends Model
 {
+    use HasFactory;
+
     public $fillable = ['code', 'title', 'detail', 'body', 'published'];
 
     public function getRouteKeyName()
---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticleFactory extends Factory
+{
+    protected $model = Article::class;
+
+    public function definition()
+    {
+        return [
+            'code' => $this->faker->countryCode(),
+            'title' => $this->faker->title(),
+            'detail' => $this->faker->randomLetter(),
+            'body' => $this->faker->text(),
+            'published' => $this->faker->boolean()
+        ];
+    }
+}
