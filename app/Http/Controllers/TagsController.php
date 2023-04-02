a52b4c7dd3349b4ef4e5187e86e895c25523e13d
semenov.d <semenov.d@introvert.bz>
2023-04-02 18:40:45 +0300

    7.6 | hidde showing not published articles in tagCloud | fix

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -9,7 +9,7 @@ class TagsController extends Controller
 {
     public function index(Tag $tag)
     {
-        $articles = $tag->articles()->with('tags')->simplePaginate(5);
+        $articles = $tag->articles()->where('published', true)->with('tags')->simplePaginate(5);
 
         return view('articles.index', compact('articles'));
     }
